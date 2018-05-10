cb2f1d10a0a8b4199e85e74b2f7b18fe958dfe29
mmaia <maia.marcos@gmail.com>
2018-05-10 14:31:57 +0200

    first push of working migrated up to exercise 1.2

--- app/src/main/java/com/codespair/sunshine/MainActivity.kt
+++ app/src/main/java/com/codespair/sunshine/MainActivity.kt
@@ -2,10 +2,37 @@ package com.codespair.sunshine
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import com.codespair.sunshine.R.id.tv_weather_data
 
-class MainActivity : Activity() {
+class MainActivity() : Activity() {
+
+  private var mWeatherTextView: TextView? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_forecast)
+
+    mWeatherTextView = findViewById(tv_weather_data)
+
+    val dummyWeatherData: Array<String> = arrayOf(
+        "Today, May 17 - Clear - 17°C / 15°C",
+        "Tomorrow - Cloudy - 19°C / 15°C",
+        "Thursday - Rainy- 30°C / 11°C",
+        "Friday - Thunderstorms - 21°C / 9°C",
+        "Saturday - Thunderstorms - 16°C / 7°C",
+        "Sunday - Rainy - 16°C / 8°C",
+        "Monday - Partly Cloudy - 15°C / 10°C",
+        "Tue, May 24 - Meatballs - 16°C / 18°C",
+        "Wed, May 25 - Cloudy - 19°C / 15°C",
+        "Thu, May 26 - Stormy - 30°C / 11°C",
+        "Fri, May 27 - Hurricane - 21°C / 9°C",
+        "Sat, May 28 - Meteors - 16°C / 7°C",
+        "Sun, May 29 - Apocalypse - 16°C / 8°C",
+        "Mon, May 30 - Post Apocalypse - 15°C / 10°C")
+
+    for(dummyWeatherDay in dummyWeatherData) {
+      mWeatherTextView?.append(dummyWeatherDay + "\n\n\n")
+    }
   }
 }
